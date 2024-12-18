de819a0a6c2bff69c36233b19a233473dc704798
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2024-12-18 17:02:05 +0200

    major updates

--- fedai/servers.py
+++ fedai/servers.py
@@ -8,27 +8,25 @@ __all__ = ['BaseServer', 'Server_mira']
 # %% ../nbs/01_servers.ipynb 3
 from copy import deepcopy
 import os
-import time
 import numpy as np
 from collections import defaultdict
 import torch
-from datetime import datetime
 from fastcore.utils import *
-from transformers import AutoTokenizer
 from peft import *
 from .models import *
 from .utils import *
-from .clients import Client_mira
+from .clients import Client_mira, BaseClient
 
 # %% ../nbs/01_servers.ipynb 5
 class BaseServer:
 
-    def __init__(self, cfg, lst_data_dict, model, holdout_ds):
+    def __init__(self, cfg, lst_data_dict, model, holdout_ds, client_class):
         self.cfg = cfg
+        self.lst_data_dict = lst_data_dict
         self.model = model
         self.holdout_ds = holdout_ds
-        self.lst_data_dict = lst_data_dict
-        self.client_list = LazyList(self, Client_mira)  # type: ignore # noqa: F405
+        self.client_list = LazyList(self, client_class)  # type: ignore # noqa: F405
+        self.latest_model_iter = dict()
         self.__str__ = self.__repr__
        
     def __str__(self) -> str:
@@ -36,20 +34,27 @@ class BaseServer:
 
 # %% ../nbs/01_servers.ipynb 7
 @patch
-def test(self: BaseServer):
-    raise NotImplementedError
+def send(self: BaseServer, client: BaseClient):  # noqa: F811
+    
+    if client.idx in self.latest_model_iter:
+        comm_round = self.latest_model_iter[client.idx]
+        model_path = os.path.join(self.cfg.output_dir, str(comm_round), 
+                                  "local_output_{}".format(client.idx),
+                                  "pytorch_model.pth")
+    else:
+        model_path = ''
 
-@patch
-def train(self: BaseServer):
-    raise NotImplementedError
-
-@patch   
-def send(self: BaseServer):
-    raise NotImplementedError
-@patch
-def aggregate(self: BaseServer):
-    raise NotImplementedError
-
+    with torch.no_grad():
+        client.model = deepcopy(self.model)
+    
+    if os.path.exists(model_path):
+        if isinstance(client.model, torch.nn.Module):
+            client.model.load_state_dict(torch.load(model_path, map_location='cpu'))
+        elif isinstance(client.model, PeftModel): # noqa: F405
+            set_peft_model_state_dict(client.model,  # noqa: F405
+                                  torch.load(model_path, map_location='cpu'),
+                                  "default")
+    return client.model
 
 # %% ../nbs/01_servers.ipynb 9
 @patch
@@ -73,11 +78,10 @@ def get_selected_client(self: BaseServer,
 
 # %% ../nbs/01_servers.ipynb 14
 class Server_mira(BaseServer):
-    def __init__(self, cfg, lst_data_dict, model, holdout_ds, **kwargs):
-        super().__init__(cfg, lst_data_dict, model, holdout_ds)
+    def __init__(self, cfg, lst_data_dict, model, holdout_ds,client_class, **kwargs):
+        super().__init__(cfg, lst_data_dict, model, holdout_ds, client_class)
         
         self.model = get_model(self.cfg)
-        self.latest_model_iter = dict()
         
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -94,45 +98,13 @@ def init_sim_matrix(self: Server_mira):
     self.alk_connection = b_symm
 
 
-# %% ../nbs/01_servers.ipynb 21
-@patch
-def send(self: Server_mira, client: Client_mira):
-    if client.idx in self.latest_model_iter:
-        comm_round = self.latest_model_iter[client.idx]
-        model_path = os.path.join(self.cfg.output_dir, str(comm_round), "local_output_{}".format(client.idx),
-                                "pytorch_model.bin")
-    else:
-        model_path = ''
-
-    with torch.no_grad():
-        client.model = deepcopy(self.model)
-    
-    if os.path.exists(model_path):
-        set_peft_model_state_dict(client.model,
-                                  torch.load(model_path, map_location='cpu'),
-                                  "default")
-        
-    return client.model
-
-# %% ../nbs/01_servers.ipynb 22
-@patch
-def save_space(self: Server_mira, client: Client_mira) -> None:
-    client.clear_model()
-    del self.model
-    del client.optimizer
-    del client
-    import gc
-    gc.collect()
-    if torch.cuda.is_available():
-        torch.cuda.empty_cache()
-
-# %% ../nbs/01_servers.ipynb 25
+# %% ../nbs/01_servers.ipynb 23
 @patch
 def aggregate(self: Server_mira, selected_clients_indices, comm_round):
     global_lr = float(self.cfg.lr) * float(self.cfg.local_step)
 
     for i, client_id in enumerate(selected_clients_indices):
-        client_path = os.path.join(self.cfg.output_dir, str(comm_round), f"local_output_{client_id}", "pytorch_model.bin")
+        client_path = os.path.join(self.cfg.output_dir, str(comm_round), f"local_output_{client_id}", "pytorch_model.pth")
         client_state_dict = torch.load(client_path, map_location=self.device)
 
         client_diff = defaultdict(lambda: torch.tensor(0.0).to(self.device))
@@ -142,7 +114,7 @@ def aggregate(self: Server_mira, selected_clients_indices, comm_round):
 
         for j, other_client_id in enumerate(selected_clients_indices):
             if i != j:
-                other_client_path = os.path.join(self.cfg.output_dir, str(comm_round), f"local_output_{other_client_id}", "pytorch_model.bin")
+                other_client_path = os.path.join(self.cfg.output_dir, str(comm_round), f"local_output_{other_client_id}", "pytorch_model.pth")
                 other_client_state_dict = torch.load(other_client_path, map_location=self.device)
 
                 weight = self.alk_connection[int(client_id)][int(other_client_id)]
@@ -155,20 +127,13 @@ def aggregate(self: Server_mira, selected_clients_indices, comm_round):
         self.update(client_state_dict, comm_round, client_id)
 
 
-# %% ../nbs/01_servers.ipynb 26
+# %% ../nbs/01_servers.ipynb 24
 @patch
 def update(self: Server_mira, client_state_dict: dict, comm_round: int, client_id: int) -> None:
     save_dir = os.path.join(self.cfg.output_dir, str(comm_round + 1), f"local_output_{client_id}")
     os.makedirs(save_dir, exist_ok=True)
-    save_path = os.path.join(save_dir, "pytorch_model.bin")
+    save_path = os.path.join(save_dir, "pytorch_model.pth")
     torch.save(client_state_dict, save_path)
-    set_peft_model_state_dict(self.model, client_state_dict, "default")
+    set_peft_model_state_dict(self.model, client_state_dict, "default")  # noqa: F405
     self.model.save_pretrained(save_dir)
 
-
-# %% ../nbs/01_servers.ipynb 29
-@patch
-def test(self: Server_mira):
-    for client in self.client_list:
-        client.eval()
-        client.save_model(self.cfg.output_dir)
