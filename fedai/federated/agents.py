37a86cb63b9e5c703cadf949846626ea8b88b81e
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2024-12-24 12:16:12 +0200

    sync

--- fedai/federated/agents.py
+++ fedai/federated/agents.py
@@ -120,7 +120,8 @@ def save_state(self: FLAgent, state_dict, comm_round):  # noqa: F811
     torch.save(state_dict, 
                os.path.join(model_path, 
                             "pytorch_model.pth"))
-    save_space(self)  # noqa: F405
+    if self.role == AgentRole.CLIENT:
+        save_space(self)
 
 
 # %% ../../nbs/02_federated.agents.ipynb 30
