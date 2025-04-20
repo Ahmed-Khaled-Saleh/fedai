35833c9ba9631f3a696c7d74c5b0ddc4b8018259
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2025-04-20 14:33:08 +0300

    FIX: model similarity is not negative

--- fedai/federated/agents.py
+++ fedai/federated/agents.py
@@ -728,7 +728,7 @@ def build_graph(self: DMTL, lst_active_ids, comm_round):
             # graph[i][j] = - (self.cfg.alpha) * w_sim + (1-self.cfg.alpha) * h_sim
             # graph[j][i] = round(graph[i][j], ndigits=3)
             # graph[i][j] = graph[j][i]
-            val = - (self.cfg.alpha) * w_sim + (1 - self.cfg.alpha) * h_sim
+            val = (self.cfg.alpha) * w_sim + (1 - self.cfg.alpha) * h_sim
             print(f"Client {id} and {other_id} similarity: {val}")
             print(f"Client {id} and {other_id} h similarity: {h_sim}")
             print(f"Client {id} and {other_id} w similarity: {w_sim}")
