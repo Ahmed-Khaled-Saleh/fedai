a930122555c72f37a3b75fcd858ca8c323398cad
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2026-02-22 16:37:54 +0200

    Debug: printing type of saved state model.

--- fedai/servers/base_server.py
+++ fedai/servers/base_server.py
@@ -215,6 +215,7 @@ def evaluate(self: BaseServer, t):
 
         res_test = client.evaluate_local(loader= 'test')
         lst_test_res.append(res_test)
+        self.logger.info(f"type of model key: {type(client_state['model'])}")
         self.state_mgr.set_state(id, client_state)
         
         del client
