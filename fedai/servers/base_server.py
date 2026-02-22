c536dbe815e2aad271719994809a785a62ba7a1b
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2026-02-22 14:20:22 +0200

    FIX: client function instantiation at eval.

--- fedai/servers/base_server.py
+++ fedai/servers/base_server.py
@@ -206,7 +206,7 @@ def evaluate(self: BaseServer, t):
     lst_test_res = []
     for id in range(self.cfg.num_clients):
         client_state = self.state_mgr.get_state(id)
-        client = self.client_fn(client_cls= self.client_cls, id= id, cfg= self.cfg, comm_round= t, client_state= client_state)
+        client = self.client_fn(id= id, comm_round= t, client_state= client_state)
         
         res_train = client.evaluate_local(loader= 'train')
         lst_train_res.append(res_train)
