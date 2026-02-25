d857b7940e68ff01b9ebe7ce9706e30b535f2afa
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2026-02-25 15:27:07 +0200

    FIX: change optimizer to Pflib one.

--- fedai/servers/perfedavg.py
+++ fedai/servers/perfedavg.py
@@ -36,7 +36,7 @@ class ServerPerFedAvg(BaseServer):
                  
         super().__init__(cfg, client_selector, client_cls, criterion, fds, writer, device, **kwargs) 
 
-# %% ../../nbs/11d_servers.perFedAvg.ipynb #6df95ca5
+# %% ../../nbs/11d_servers.perFedAvg.ipynb #b4be574f
 @patch
 def aggregate(self: ServerPerFedAvg, lst_active_ids, comm_round, len_clients_ds):
     # m_t = sum(len_clients_ds.values())
