50fc2eef93ae72e0eb5e5ecca957ca2494e11cc2
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2026-03-16 17:32:20 +0200

    FIX: fix the seed for now to evaluate on the same susbets across all methods.

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ def main(cfg: MainConfig):
 
     cfg.now = time.strftime("%Y-%m-%d_%H-%M-%S")
     cfg.partitioner.num_partitions = cfg.num_clients
-    cfg.random_seed = np.random.randint(0, 100000)
+    cfg.random_seed = 42#np.random.randint(0, 100000)
 
     fds = init_data(cfg)
     client_selector = BaseClientSelector(cfg)
