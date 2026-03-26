FIX: resnet 's batch norms should not be part of the computation of the aggregate function and should be skipped.

fedai/servers/fedavg.py
@@ -44,26 +44,28 @@ class ServerFedAvg(BaseServer):
 @patch
 def aggregate(self: ServerFedAvg, lst_active_ids, comm_round, len_clients_ds):
     m_t = sum(len_clients_ds.values())
-    
     with torch.no_grad():
         global_model = None
-        
         for id in lst_active_ids:
             client_state_dict = self.state_mgr.get_state(id).get('model', None)
-
             if global_model is None:
                 global_model = {k: torch.zeros_like(v) for k, v in client_state_dict.items()}
-
             n_k = len_clients_ds[id]
             weight = n_k / m_t
             for key in global_model.keys():
+                if not client_state_dict[key].is_floating_point():
+                    continue
                 global_model[key].add_(client_state_dict[key], alpha=weight)
+
+        # copy integer buffers directly from last client (they should all be the same)
+        for key in global_model.keys():
+            if not global_model[key].is_floating_point():
+                global_model[key].copy_(client_state_dict[key])
 
         self.model.load_state_dict(global_model)
 
         for id in lst_active_ids:
             self.state_mgr.set_state(id, {
-                    'model': self.model.state_dict(),
-                    'optimizer': self.state_mgr.get_state(id).get('optimizer', None),
+                'model': self.model.state_dict(),
+                'optimizer': self.state_mgr.get_state(id).get('optimizer', None),
             })
-        

fedai/servers/fedu.py
@@ -46,7 +46,7 @@ class ServerFedu(BaseServer):
         b_symm[b_symm < 0.25] = 0
         self.alk_connection = b_symm
 
-# %% ../../nbs/11i_servers.fedu.ipynb #bab85d32
+# %% ../../nbs/11i_servers.fedu.ipynb #bc01f93d
 @patch
 def aggregate(self: ServerFedu, lst_active_ids, comm_round, len_clients_ds):
 
@@ -56,9 +56,6 @@ def aggregate(self: ServerFedu, lst_active_ids, comm_round, len_clients_ds):
     with torch.no_grad():
         aggregated_states = []
         for i, id in enumerate(lst_active_ids):
-            # state_path = os.path.join(self.cfg.save_dir, str(comm_round), f"local_output_{id}", "state.pth")
-            # state = torch.load(state_path, weights_only= False)
-            # client_state_dict = state['model']
             client_state = self.state_mgr.get_state(id)
             client_state_dict = client_state['model']
 
@@ -70,22 +67,18 @@ def aggregate(self: ServerFedu, lst_active_ids, comm_round, len_clients_ds):
             for j, other_id in enumerate(lst_active_ids):
                 if i == j:
                     continue
-                # other_state_path = os.path.join(self.cfg.save_dir, str(comm_round), f"local_output_{other_id}", "state.pth")
-                
-                # other_state = torch.load(other_state_path, weights_only= False)
-                # other_state_dict = other_state['model']
                 other_state_dict = self.state_mgr.get_state(other_id).get('model', None)
 
                 weight = self.alk_connection[int(id)][int(other_id)]
                 for key in client_state_dict.keys():
+                    if not client_state_dict[key].is_floating_point():
+                        continue
                     client_diff[key].add_(weight * (client_state_dict[key] - other_state_dict[key]))
 
             for key in client_state_dict:
+                if not client_state_dict[key].is_floating_point():
+                    continue
                 client_state_dict[key].sub_(global_lr * reg_param * client_diff[key])
-
-            # clinet_state = {
-            #     'model': client_state_dict,
-            # }
 
             clinet_state = {
                     'model': client_state_dict,
@@ -93,12 +86,5 @@ def aggregate(self: ServerFedu, lst_active_ids, comm_round, len_clients_ds):
                 }
             aggregated_states.append(clinet_state)
 
-            # agg_client_state_path = os.path.join(self.cfg.save_dir, str(comm_round), f"aggregated_model_{id}", "state.pth")
-            
-            # if not os.path.exists(os.path.dirname(agg_client_state_path)):
-            #     os.makedirs(os.path.dirname(agg_client_state_path))
-
-            # torch.save(clinet_state, agg_client_state_path)
-
         for i, id in enumerate(lst_active_ids):
-            self.state_mgr.set_state(id, aggregated_states[i])  
+            self.state_mgr.set_state(id, aggregated_states[i])

fedai/servers/pfedme.py
@@ -81,38 +81,40 @@ def client_fn(self: ServerpFedMe, id, comm_round, client_state):
     return client
 
 
-# %% ../../nbs/11f_servers.pfedme.ipynb #0005248d
+# %% ../../nbs/11f_servers.pfedme.ipynb #79c2c7d2
 @patch
 def aggregate(self: ServerpFedMe, lst_active_ids, comm_round, len_clients_ds):
-
     m_t = sum(len_clients_ds.values())
     with torch.no_grad():
-
         prev_global_model = self.model.state_dict()
         for i, id in enumerate(lst_active_ids):
-            # state_path = os.path.join(self.cfg.save_dir, str(comm_round), f"local_output_{id}", "state.pth")
-            # state = torch.load(state_path, weights_only=False)
             state = self.state_mgr.get_state(id)
             client_state_dict = state['model']
-
             if i == 0:
                 global_model = {
                     key: torch.zeros_like(value) 
                     for key, value in client_state_dict.items()
                 }
-
             n_k = len_clients_ds[id]
-            weight =  n_k / m_t 
-
+            weight = n_k / m_t
             for key in client_state_dict.keys():
+                if not client_state_dict[key].is_floating_point():
+                    continue
                 global_model[key].add_(weight * client_state_dict[key])
 
         for key in global_model.keys():
-            global_model[key].copy_((1-self.cfg.algorithm.beta)*global_model[key] + self.cfg.algorithm.beta*prev_global_model[key])
+            if not global_model[key].is_floating_point():
+                # copy integer buffers directly from prev global model
+                global_model[key].copy_(prev_global_model[key])
+                continue
+            global_model[key].copy_(
+                (1 - self.cfg.algorithm.beta) * global_model[key] 
+                + self.cfg.algorithm.beta * prev_global_model[key]
+            )
 
         self.model.load_state_dict(global_model)
-        
-    for i, id in enumerate(lst_active_ids):
-        client_state = self.state_mgr.get_state(id)
-        client_state['model'] = global_model
-        self.state_mgr.set_state(id, client_state)
+
+        for i, id in enumerate(lst_active_ids):
+            client_state = self.state_mgr.get_state(id)
+            client_state['model'] = global_model
+            self.state_mgr.set_state(id, client_state)